7eed2820d85b9d06d5bab4445eae9a69992f38e9
Pragyan Prakhar <pragyanprakhar@gmail.com>
2024-07-11 17:15:10 +0530

    Initialized the database

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const ejs = require("ejs");
+const Listing = require("./models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/HavenQuest";
 main()
     .then(() => {
         console.log("Connected to DB");
     })
-    .catch((err) => { 
+    .catch((err) => {
         console.log(err);
     });
 async function main() {
@@ -19,6 +20,19 @@ async function main() {
 app.get("/", (req, res) => {
     res.send("Hi, I am root!");
 });
+app.get("/testListing", async (req, res) => {
+    let sampleListing = new Listing({
+        title: "My new Villa",
+        description: "By the beach",
+        price: 1200,
+        location:"Calangute Goa",
+        country:"India",
+    });
+    await sampleListing.save();
+    console.log("Sample was saved");
+    res.send("Successful Testing");
+});
+
 app.listen(8080, () => {
     console.log("Server is listening to the port 8080");
 });
