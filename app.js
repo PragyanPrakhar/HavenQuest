3697847850e894c6fa58ec3730507fb9b8eeced4
Pragyan Prakhar <pragyanprakhar@gmail.com>
2024-07-16 15:09:20 +0530

    Completed the intial styling

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ const Listing = require("./models/listing.js");
 const MONGO_URL = "mongodb://127.0.0.1:27017/HavenQuest";
 const path = require("path");
 const methodOverride = require("method-override");
-app.use(methodOverride("_method"))
+const ejsMate = require("ejs-mate");
+app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 main()
     .then(() => {
         console.log("Connected to DB");
@@ -34,8 +37,8 @@ app.get("/listings", async (req, res) => {
 });
 
 //New Route
-app.get("/listings/new",(req,res)=>{
-    res.render("listings/new.ejs")
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
 });
 
 //Show Route
@@ -46,37 +49,49 @@ app.get("/listings/:id", async (req, res) => {
 });
 
 //Create Route
-app.post("/listings",async(req,res)=>{
-    let {title,description,image,price,country,location} = req.body;
-const newListing=new Listing({title,description,image,price,country,location});
+app.post("/listings", async (req, res) => {
+    let { title, description, image, price, country, location } = req.body;
+    const newListing = new Listing({
+        title,
+        description,
+        image,
+        price,
+        country,
+        location,
+    });
     await newListing.save();
     res.redirect("/listings");
-})
+});
 
 // Edit Route
 app.get("/listings/:id/edit", async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs",{listing});
+    res.render("listings/edit.ejs", { listing });
 });
 
 //Update Route
 app.put("/listings/:id", async (req, res) => {
     let { id } = req.params;
-    let {title,description,image,price,country,location} = req.body;
+    let { title, description, image, price, country, location } = req.body;
 
-   await  Listing.findByIdAndUpdate(id,{title,description,image,price,country,location});
-   res.redirect("/listings")
+    await Listing.findByIdAndUpdate(id, {
+        title,
+        description,
+        image,
+        price,
+        country,
+        location,
+    });
+    res.redirect("/listings");
 });
 
 //Delete Route
-app.delete("/listings/:id",async(req,res)=>{
-    let {id}=req.params;
-    let deletedList=await Listing.findByIdAndDelete(id)
-    res.redirect("/listings"); 
-})
+app.delete("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    let deletedList = await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+});
 app.listen(8080, () => {
     console.log("Server is listening to the port 8080");
 });
-
-
